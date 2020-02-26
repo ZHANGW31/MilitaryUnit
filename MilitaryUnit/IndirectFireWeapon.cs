798cd85544a5d927c74669b6079b1ad6b81bbc2e
Wei Zhang <wei.zhang4@outlook.com>
2020-02-26 11:22:23 -0800

    Add project files.

---
+++ MilitaryUnit/IndirectFireWeapon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryUnit
+{
+    class IndirectFireWeapon : Weapon
+    {
+    }
+}
---
+++ MilitaryUnit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MilitaryUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
