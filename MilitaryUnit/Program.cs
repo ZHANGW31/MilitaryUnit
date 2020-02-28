82d1f8f2945e29af262f2b29f3aff48fa702172e
Wei Zhang <wei.zhang4@outlook.com>
2020-02-28 14:56:38 -0800

    added a private field, and a constructor for the rifle class.

--- MilitaryUnit/Program.cs
+++ MilitaryUnit/Program.cs
@@ -6,9 +6,19 @@ namespace MilitaryUnit
     {
         static void Main(string[] args)
         {
-            Mission blueFalcon = new Mission();
-            blueFalcon.MissionName = "blue falcon";
-            Console.WriteLine(blueFalcon.MissionName);
+            Console.WriteLine("MilitaryUnit");
+
+            Rifle m16 = new Rifle("5.56");
+            m16.WeaponName = "M16";
+
+            Console.WriteLine("\nThe " + m16.WeaponName + " is a rifle");
+            m16.WeaponEmployment();
+            m16.EffectiveRange("550 meters");
+            m16.CarryStyle("Sling");
+            m16.RifleAmmoTypes();
+            m16.RifleMountingOptions();
+
+
         }
     }
 }
--- MilitaryUnit/Weapon/Rifle.cs
+++ MilitaryUnit/Weapon/Rifle.cs
@@ -6,17 +6,23 @@ namespace MilitaryUnit
 {
     class Rifle : Weapon
     {
+        private string ammoType;
+
+        public Rifle(string ammo)
+        {
+            ammoType = ammo;
+        }
         public override void WeaponEmployment()
         {
-            Console.WriteLine("Distanced Engagements, Suppressing Fire");
+            Console.WriteLine("It is used for: Distanced Engagements, Suppressing Fire");
         }
         public void RifleAmmoTypes()
         {
-            Console.WriteLine("Standard NATO Rifle Ammunition Cartridges are: 5.56 x 45mm, 7.62 x 51mm");
+            Console.WriteLine("It uses: " + ammoType + " ammunition.");
         }
         public void RifleMountingOptions()
         {
-            Console.WriteLine("Scope, Bipod, Rifle Sights, Laser Pointers, Flash Lights, Grenade Launcher");
+            Console.WriteLine("Mounting options: Scope, Bipod, Rifle Sights, Laser Pointers, Flash Lights, Grenade Launcher");
         }
 
     }
